6d1aabc26b82dd63f197ec8fd4994ca1c029e388
elrober1996 <roberto.dejesus6@upr.edu>
2019-04-20 17:29:09 -0400

    Partial implementation; character, delimiter, operator, and keyword does not work.

--- lexer.py
+++ lexer.py
@@ -7,14 +7,30 @@ tokens = [
     'FLOAT'
     'CHARACTER',
     'DELIMITER',
-    'OPERATOR',
+    'PLUS',
+    'MINUS',
+    'DIVIDE',
+    'MULTIPLY',
+    'EQUALS',
+    'LESS_THAN',
+    'MORE_THAN',
+    'LESS_EQUAL',
+    'MORE_EQUAL',
+    'NOT_EQUAL',
+    'EQUALS'
     'KEYWORD',
 ]
 
 t_ID = '[a-zA-z_][a-zA-z_0-9]+'
-t_CHARACTER = '[a-zAz]'
-t_DELIMITER = [r'\+', r'\-', ]
-t_OPETATOR = r'\[+-*/=]'
+# t_CHARACTER = '[a-zAz]'
+# t_DELIMITER = r'\+', r'\-'
+# t_OPETATOR = r'\[+-*/=]'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_EQUALS = r'\='
+# t_KEYWORD = '(Board|Player|Dice|Timer)'
 
 t_ignore = r' ' # We are going to ignore spaces
 
@@ -23,7 +39,7 @@ def t_FLOAT(t):
     t.value = float(t.value)
     return t
 
-def INT(t):
+def t_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
@@ -33,4 +49,13 @@ def t_error(t):
     print('Ilegal argument!')
     lexer.skip(1)
 
+
 lexer = lex.lex()
+
+# lexer.input("abc = 1")
+#
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
